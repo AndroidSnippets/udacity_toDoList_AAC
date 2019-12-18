6e0311443f1a2efd1bb9243e560915230991062a
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2019-12-18 23:47:08 +0200

    add Database class

---
+++ app/src/main/java/com/example/android/todolist/database/AppDatabase.java
@@ -0,0 +1,33 @@
+package com.example.android.todolist.database;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+public abstract class AppDatabase extends RoomDatabase {
+    private static final String TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "toDoList";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context){
+        if(sInstance == null) {
+            synchronized (LOCK){
+                Log.d(TAG, "getInstance: Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class,
+                            AppDatabase.DATABASE_NAME).build();
+            }
+
+        }
+        Log.d(TAG, "getInstance: getting the database instance");
+        return sInstance;
+    }
+
+}
